49cd836cbe3d01249d7b773c6b2252bbb9231075
SANDEEP T V <65756151+Sandeeptv2002@users.noreply.github.com>
2022-04-15 22:04:10 +0530

    Update email.cpp

--- email.cpp
+++ email.cpp
@@ -1,15 +1,42 @@
+#include <iostream>
 #include <CkMailMan.h>
 #include <CkEmail.h>
-void ChilkatSample(void)
-    {
-    CkMailMan mailman;
+using namespace std;
+
+int main()
+{
+    string user;
+    cin >> user;
+    cout << endl;
+
+    string from;
+    cin >> from;
+    cout << endl;
+
+    string pass;
+    cin >> pass;
+    cout << endl;
+
+    string to;
+    cin >> to;
+    cout << endl;
+
+    string subject;
+    cin >> subject;
+    cout << endl;
+
+    string body;
+    cin >> body;
+    cout << endl;
+
+    kMailMan mailman;
     mailman.put_SmtpHost("smtp.google.com");
-    mailman.put_SmtpUsername("SandeepTV");
-    mailman.put_SmtpPassword("123456");
+    mailman.put_SmtpUsername(user);
+    mailman.put_SmtpPassword(pass);
     CkEmail email;
-    email.put_Subject("simple text message");
-    email.put_Body("Hello all I’m Sandeep");
-    bool success = email.AddTo("Sandeep ","tvsandeep2002@gmail.com");
+    email.put_Subject(subject);
+    email.put_Body(body);
+    bool success = email.AddTo(from,to);
     success = mailman.SendEmail(email);
     if (success != true) {
         std::cout << mailman.lastErrorText() << "\r\n";
@@ -21,4 +48,5 @@ void ChilkatSample(void)
     }
 
     std::cout << "Mail Sent!" << "\r\n";
-    } 
+    return '0';
+}
